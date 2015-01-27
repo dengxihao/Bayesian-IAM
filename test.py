216ad343b368b888f0e25aeff1e8d638863a09a0
dengxihao <xud104@psu.edu>
2015-01-26 22:44:16 -0500

    c

--- test.py
+++ test.py
@@ -9,13 +9,19 @@ from Input import t1, t0, ind, dPop, dGDP, dCO2, kappa, parb
 
 
 def main(): 
+##
+## main function generates mcmc samples
+##
 
+    # initial parameters
       par0=np.loadtxt('parm1.txt')      
-
+    
+    # size of samples, burn-in and thin-in
       Nsamp = 5000000
       Nburn = 100000
       Nthin = 1000        
-                
+    
+    # parallel generating 10 mcmc chains            
       res = Parallel(n_jobs=10)(delayed(mh)(Nsamp, Nburn, Nthin, t1, t0, ind, dPop, dGDP, dCO2, parb, kappa, par0) for i in range(10))  
 
       samps, fPop, fGDP, fCO2, accept = zip(*res)  
